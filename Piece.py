b8cb9e31ce9abd059bb52005583c207eda0afbfb
Teeedor <ted123810@gmail.com>
2022-03-09 13:41:38 -0700

    Adding the piece class for later use

---
+++ Piece.py
@@ -0,0 +1,10 @@
+# This file defines the class for a piece
+
+class Piece():
+    def __init__(self, incolor, intype, xpos, ypos):
+        self.color = incolor
+        self.type = intype
+        self.x = xpos
+        self.y = ypos
+
+    # Implement move() here
