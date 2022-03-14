abc4f25afd569d7d0d6823fa1162e925239d5b30
Teeedor <ted123810@gmail.com>
2022-03-14 15:04:39 -0700

    added the screen clearing after each move.

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ def main():
     # MAIN LOGIC
     # before the game Starts
     visual = True
-    board = create_pieces(1)
+    board = create_pieces(3)
     turn = 'w'  # game starts with white
     while no_winner(board):  # While the game is running
         if visual:
+            clear()  # this makes playing much more enjoyable.
             display_board(board)
         ma = read_move(board, turn)  # Read in the move from the Player (x1, y1, x2, y2)
         valid_move, board = move(board, ma[0], ma[1], ma[2], ma[3])  # attempt to make the move
         if valid_move:
             turn = rotate_turn(turn)  # At the end of the turn, rotate it
         else:
-            print("not a valid move, try again!")
+            print("NOT A VALID MOVE, TRY AGAIN!")
 
 
 main()
